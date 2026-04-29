10bb7d77963b124b9b68c46a4139a505f1d4cdf9
Lee DaSan <ddmt0805@gmail.com>
2026-04-30 00:05:40 +0900

    board서비스, update 구현 및 select문 mybatis관련 트러블슈팅

---
+++ BoardCRUD/src/com/kh/exception/NotEqualsPasswordException.java
@@ -0,0 +1,30 @@
+package com.kh.exception;
+
+public class NotEqualsPasswordException extends RuntimeException {
+
+	public NotEqualsPasswordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotEqualsPasswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotEqualsPasswordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotEqualsPasswordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotEqualsPasswordException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
